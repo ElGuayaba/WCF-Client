f029277f79dbae96f2fc98b3ccaa3faf942e9992
Jose Alamo <alamose@hotmail.com>
2018-08-06 17:36:29 +0200

    first commit

---
+++ ClienteWcfData/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteWcfData
+{
+	public partial class Form1 : Form
+	{
+		Form2 form;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			form = new Form2();
+			form.AlumnoAdded += new EventHandler(this.button3_Click);
+			form.Show();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			ServiceReference1.Service1Client svc = new ServiceReference1.Service1Client("HTTP");
+			List<ServiceReference1.Alumno> listaAlumnos = svc.GetAll();
+			dgvAlumno.DataSource = listaAlumnos;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			ServiceReference1.Service1Client svc = new ServiceReference1.Service1Client("TCP");
+			List<ServiceReference1.Alumno> listaAlumnos = svc.GetAll();
+			dgvAlumno.DataSource = listaAlumnos;
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ ClienteWcfData/Form2.cs
@@ -0,0 +1,33 @@
+﻿using ClienteWcfData.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteWcfData
+{
+	public partial class Form2 : Form
+	{
+		public event EventHandler AlumnoAdded;
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		public void button1_Click(object sender, EventArgs e)
+		{
+			Alumno alumno = new Alumno();
+			alumno.Nombre = textBox1.Text;
+			alumno.Apellidos = textBox2.Text;
+			Service1Client svc = new Service1Client("TCP");
+			svc.Add(alumno);
+			AlumnoAdded(this, e);
+			Close();
+		}
+	}
+}
